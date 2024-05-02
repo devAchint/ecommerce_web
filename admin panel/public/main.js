a85c6a4559cc0611aa2acadfe8051a84e43651a7
achint2093 <=achintwadhwa418@gmail.com>
2024-05-02 15:59:26 +0530

    admin email and password stored in firebase

--- admin panel/public/main.js
+++ admin panel/public/main.js
@@ -1,16 +1,38 @@
 function login() {
+  var adminemail, adminpass;
   var email = document.getElementById("login-email").value;
   var password = document.getElementById("login-password").value;
 
-  if (email === '' || password === '') {
-    alert("Input is blank!");
-  } else if (email != "admin@gmail.com" && password!="admin2093@") {
-    alert("wrong user id and password");
-  } else {
-    showLoading();
-    localStorage.setItem('isAdminLoggedIn', 'true');
-    window.location.href = 'pages/category.html';
-  }
+  const dbRef = firebase.database().ref("admin");
+
+  dbRef
+    .once("value")
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        adminemail = data.email;
+        adminpass = data.password;
+        if (adminemail != "" || adminpass != "") {
+          if (email === "" || password === "") {
+            alert("Input is blank!");
+          } else if (email === adminemail && password === adminpass) {
+            showLoading();
+            localStorage.setItem("isAdminLoggedIn", "true");
+            window.location.href = "pages/category.html";
+          
+          } else {
+            alert("wrong user id and password");
+          }
+        }else{
+          alert("wait and try again");
+        }
+      } else {
+        alert("No user found");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching data:", error);
+    });
 }
 
 document.getElementById("submit").onclick = login;
